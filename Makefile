68ee5da51c5e7af701d0efc58d05588f66b77181
d61h6k4 <ddbihbka@gmail.com>
2021-02-17 09:18:41 +0100

    Added calculator to dump detections to file

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
+VIDEO_NAME = vfb_luebeck_012500_3min
+
 build:
 	bazel build --define MEDIAPIPE_DISABLE_GPU=1 //runner:ap_demo
 
 debug: build
-	GLOG_logtostderr=1 bazel-bin/runner/ap_ice_hockey \
-       --calculator_graph_config_file runner/ap_ice_hockey_debug_graph.pbtxt \
-       --input_side_packets=input_video_path=test_videos/primary_vod_pano.mp4,output_file_path=rois.csv,output_detections_file_path=detections.csv
+	GLOG_logtostderr=1 bazel-bin/runner/ap_demo \
+       --calculator_graph_config_file runner/ap_demo_debug_graph.pbtxt \
+       --input_side_packets=input_video_path=test_videos/$(VIDEO_NAME).mp4,output_detections_file_path=$(VIDEO_NAME)_detections.csv
 
 render: build
 	GLOG_logtostderr=1 bazel-bin/runner/ap_demo \
        --calculator_graph_config_file runner/ap_demo_graph.pbtxt \
-       --input_side_packets=input_video_path=test_videos/mab8a08339_012500_10min.mp4,output_video_path=rois.mp4
+       --input_side_packets=input_video_path=test_videos/$(VIDEO_NAME).mp4,output_video_path=processed_videos/$(VIDEO_NAME).mp4
 
--- runner/calculators/autoproduction_dump_calculator.cc
+++ runner/calculators/autoproduction_dump_calculator.cc
@@ -11,43 +11,47 @@
 namespace mediapipe {
 namespace {
 constexpr char kRectTag[] = "RECT";
-constexpr char kDetectionsTag[] = "DETECTIONS";
+constexpr char kPersonsDetection[] = "PERSONS_DETECTION";
+constexpr char kSportBallsDetection[] = "SPORTBALLS_DETECTION";
+
 constexpr char kOutputFileTag[] = "OUTPUT_FILE_PATH";
 constexpr char kOutputDetectionsFileTag[] = "OUTPUT_DETECTIONS_FILE_PATH";
 
 int TimestampToFrameNum(::mediapipe::Timestamp t) {
   return static_cast<int>(t.Microseconds() / 40000.);
 }
-}  // namespace
+} // namespace
 REGISTER_CALCULATOR(AutoproductionDumpCalculator);
 
-::mediapipe::Status AutoproductionDumpCalculator::GetContract(
-    CalculatorContract* cc) {
-  cc->Inputs().Tag(kRectTag).Set<Rect>();
-  cc->InputSidePackets().Tag(kOutputFileTag).Set<std::string>();
-
-  RET_CHECK(!(cc->Inputs().HasTag(kDetectionsTag) ^
-              cc->InputSidePackets().HasTag(kOutputDetectionsFileTag)))
-      << "DETECTIONS and OUTPUT_DETECTIONS_FILE_PATH shoud be setted together";
+::mediapipe::Status
+AutoproductionDumpCalculator::GetContract(CalculatorContract *cc) {
+  if (cc->InputSidePackets().HasTag(kOutputFileTag)) {
+    cc->Inputs().Tag(kRectTag).Set<Rect>();
+    cc->InputSidePackets().Tag(kOutputFileTag).Set<std::string>();
+  }
 
   if (cc->InputSidePackets().HasTag(kOutputDetectionsFileTag)) {
     cc->InputSidePackets().Tag(kOutputDetectionsFileTag).Set<std::string>();
-    cc->Inputs().Tag(kDetectionsTag).Set<std::vector<Detection>>();
+    cc->Inputs().Tag(kPersonsDetection).Set<std::vector<Detection>>();
+    cc->Inputs().Tag(kSportBallsDetection).Set<std::vector<Detection>>();
   }
 
   return ::mediapipe::OkStatus();
 }
 
-::mediapipe::Status AutoproductionDumpCalculator::Open(CalculatorContext* cc) {
-  const auto& filename =
-      cc->InputSidePackets().Tag(kOutputFileTag).Get<std::string>();
-  dumper_.open(filename, std::ios::out);
-  RET_CHECK(dumper_.is_open()) << "Failed to open file " << filename;
-  // Write header
-  dumper_ << "frame_num,x,y,w,h\n";
+::mediapipe::Status AutoproductionDumpCalculator::Open(CalculatorContext *cc) {
+  if (cc->InputSidePackets().HasTag(kOutputFileTag)) {
+
+    const auto &filename =
+        cc->InputSidePackets().Tag(kOutputFileTag).Get<std::string>();
+    dumper_.open(filename, std::ios::out);
+    RET_CHECK(dumper_.is_open()) << "Failed to open file " << filename;
+    // Write header
+    dumper_ << "frame_num,x,y,w,h\n";
+  }
 
   if (cc->InputSidePackets().HasTag(kOutputDetectionsFileTag)) {
-    const auto& dfilename =
+    const auto &dfilename =
         cc->InputSidePackets().Tag(kOutputDetectionsFileTag).Get<std::string>();
     detections_.open(dfilename, std::ios::out);
     RET_CHECK(detections_.is_open()) << "Failed to open file " << dfilename;
@@ -58,20 +62,38 @@ REGISTER_CALCULATOR(AutoproductionDumpCalculator);
   return ::mediapipe::OkStatus();
 }
 
-::mediapipe::Status AutoproductionDumpCalculator::Process(
-    CalculatorContext* cc) {
-  if (!cc->Inputs().Tag(kRectTag).IsEmpty()) {
-    const auto& rect = cc->Inputs().Tag(kRectTag).Get<Rect>();
+::mediapipe::Status
+AutoproductionDumpCalculator::Process(CalculatorContext *cc) {
+  if (cc->Inputs().HasTag(kRectTag) && !cc->Inputs().Tag(kRectTag).IsEmpty()) {
+    const auto &rect = cc->Inputs().Tag(kRectTag).Get<Rect>();
     dumper_ << TimestampToFrameNum(cc->InputTimestamp()) << ','
             << rect.x_center() << ',' << rect.y_center() << ',' << rect.width()
             << ',' << rect.height() << '\n';
   }
 
-  if (cc->Inputs().HasTag(kDetectionsTag) &&
-      !cc->Inputs().Tag(kDetectionsTag).IsEmpty()) {
-    const auto& detections =
-        cc->Inputs().Tag(kDetectionsTag).Get<std::vector<Detection>>();
-    for (const auto& detection : detections) {
+  if (cc->Inputs().HasTag(kPersonsDetection) &&
+      !cc->Inputs().Tag(kPersonsDetection).IsEmpty()) {
+    const auto &detections =
+        cc->Inputs().Tag(kPersonsDetection).Get<std::vector<Detection>>();
+    for (const auto &detection : detections) {
+      detections_ << TimestampToFrameNum(cc->InputTimestamp()) << ','
+                  << detection.location_data().relative_bounding_box().ymin()
+                  << ','
+                  << detection.location_data().relative_bounding_box().xmin()
+                  << ','
+                  << detection.location_data().relative_bounding_box().width()
+                  << ','
+                  << detection.location_data().relative_bounding_box().height()
+                  << ',' << detection.label().Get(0) << ','
+                  << detection.score().Get(0) << '\n';
+    }
+  }
+
+  if (cc->Inputs().HasTag(kSportBallsDetection) &&
+      !cc->Inputs().Tag(kSportBallsDetection).IsEmpty()) {
+    const auto &detections =
+        cc->Inputs().Tag(kSportBallsDetection).Get<std::vector<Detection>>();
+    for (const auto &detection : detections) {
       detections_ << TimestampToFrameNum(cc->InputTimestamp()) << ','
                   << detection.location_data().relative_bounding_box().ymin()
                   << ','
@@ -87,9 +109,9 @@ REGISTER_CALCULATOR(AutoproductionDumpCalculator);
   return ::mediapipe::OkStatus();
 }
 
-::mediapipe::Status AutoproductionDumpCalculator::Close(CalculatorContext* cc) {
+::mediapipe::Status AutoproductionDumpCalculator::Close(CalculatorContext *cc) {
   dumper_.close();
   detections_.close();
   return ::mediapipe::OkStatus();
 }
-}  // namespace mediapipe
+} // namespace mediapipe
